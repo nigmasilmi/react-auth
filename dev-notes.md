9d140d71ed9ac3ae0c90226706a7653202848a64
Nigma Silmi <sn.silmi@gmail.com>
2022-04-04 09:03:50 -0400

    Token expiration management part 1

--- dev-notes.md
+++ dev-notes.md
@@ -15,3 +15,23 @@ Includes dev notes
 ### Navigation Guards
 
 - Mechanism to protect access to routes based on authentication
+
+### The purpose of tokens
+
+- Tokens are like a temporary permit than contains the thing that they are giving you permission for and your identification data in some way.
+
+- The generation of that permit is responsability of the backend, some API.
+
+- If we use Firebase as backend, there are endpoints in the API that allows you to access to that permit, if you ask properly for it and if you fill the requirements.
+
+- The response of that request contains the token and the time until the token expires. Like the initial number of a count-back.
+
+- There is no point for the user to use that same token because the backend won't grant permissions with an expired token.
+
+- We can tell the user that they need to identify again (log in) by showing them the login page.
+
+- We know when to show the login page because if we know the time in the future that this token expires, then we can decide if showing the login page or not.
+
+- Remember that if the user is not logged in, they can not see some restricted routes. Just the initial one (/auth).
+
+- So, what is the solution? log the user out when their token has expired.
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,23 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 const AuthContext = React.createContext({
-  token: '',
+  token: "",
   isLoggedIn: false,
   login: (token) => {},
   logout: () => {},
 });
 
+const calculateRemainingTime = (expirationTime) => {
+  const currentTime = new Date().getTime();
+  // timestamp of the time in the future as string
+  // this timestamp is calculated in the then function waiting for the response
+  const adjustedExpTime = new Date(expirationTime).getTime();
+  const remainingDuration = adjustedExpTime - currentTime;
+  return remainingDuration;
+};
+
 // this is the component passes the context
 // and the context itself
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  const initialToken = localStorage.getItem("token");
+  const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token;
-  const loginHandler = (token) => {
-    // validación y retorno de respuesta
-    setToken(token);
-  };
   const logOutHandler = () => {
     setToken(null);
+    localStorage.removeItem("token");
+  };
+
+  const loginHandler = (token, expirationTime) => {
+    // the journey begins here
+    localStorage.setItem("token", token);
+    // validación y retorno de respuesta
+    setToken(token);
+
+    // how much time does the token have left?
+    const remainingTime = calculateRemainingTime(expirationTime);
+
+    // start the timer
+    setTimeout(logOutHandler, remainingTime);
   };
 
   const contextValue = {
